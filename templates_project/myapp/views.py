6dade21c0ad3f99129d01d18bb62745a4d419e08
Rushikesh12 <rushikeshtaur12@gmail.com>
2023-06-24 17:26:27 +0530

    commited

---
+++ templates_project/myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    dict={'insert_me':'Hello EveryOne'}
+    return render(request,'myapp/home.html',context=dict)
+
+def comment(request):
+    dict={'insert_me':'Comment Section'}
+    return  render(request,'myapp/comments.html',context=dict)
+
+def like(request):
+    dict = {'insert_me': 'Like Section'}
+    return render(request, 'myapp/comments.html', context=dict)
+def story(request):
+    dict = {'insert_me': 'Story Section'}
+    return render(request, 'myapp/comments.html', context=dict)
+
+
+# Create your views here.
