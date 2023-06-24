55d7bec9aa1b9c327ecdbf5da73e48f716d64826
Rushikesh12 <rushikeshtaur12@gmail.com>
2023-06-24 18:01:46 +0530

    commited

--- templates_project/templates_project/urls.py
+++ templates_project/templates_project/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from myapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('comment/',views.comment,name='comment'),
-    path('like/',views.like,name='like'),
-    path('story/',views.story,name='story'),
+    path('', views.home, name='home'),
+    path('comment/', views.comment, name='comment'),
+    path('like/', views.like, name='like'),
+    path('story/', views.story, name='story'),
 ]
